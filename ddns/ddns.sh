23cad44f8aa960dfc242f9a20c82f08829befc7e
Mike Tovino <mike.tovino@gmail.com>
2024-01-12 10:45:01 -0800

    Update ddns.sh

--- ddns/ddns.sh
+++ ddns/ddns.sh
@@ -1,10 +1,5 @@
 #!/bin/bash -ex
 
-IFNAME=enp3s0
-
-IP=$(ip addr show dev $IFNAME | grep "inet " | cut -d" " -f6)
-let IP_SLASH_POS=`expr index "$IP" /`
-let IP_SLASH_POS=$IP_SLASH_POS-1
-IP=${IP:0:${IP_SLASH_POS}}
+IP=$(curl https://api.ipify.org -sS)
 
 curl --verbose "https://dynamicdns.park-your-domain.com/update?host=${HOSTNAME}&domain=${DDNS_DOMAIN}&password=${DDNS_PASSWORD}&ip=${IP}"
