a20041d43187a257bf770abf4b5c32641ac67e6c
Mike Tovino <mike.tovino@gmail.com>
2024-01-12 11:32:46 -0800

    Update ddns.sh

--- ddns/ddns.sh
+++ ddns/ddns.sh
@@ -1,5 +1,10 @@
 #!/bin/bash -ex
 
+DDNS_DOMAIN='tovino.com'
+DDNS_PASSWORD='TODO'
+DNS_HOST='TODO'
 IP=$(curl https://api.ipify.org -sS)
 
-curl --verbose "https://dynamicdns.park-your-domain.com/update?host=${HOSTNAME}&domain=${DDNS_DOMAIN}&password=${DDNS_PASSWORD}&ip=${IP}"
+logger "ddns: https://dynamicdns.park-your-domain.com/update?host=${DNS_HOST}&domain=${DDNS_DOMAIN}&password=${DDNS_PASSWORD}&ip=${IP}"
+OUT=$(curl "https://dynamicdns.park-your-domain.com/update?host=${DNS_HOST}&domain=${DDNS_DOMAIN}&password=${DDNS_PASSWORD}&ip=${IP}" -sS)
+logger "${OUT}"
